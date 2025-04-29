5118c01f9c2f7e774872901700def8c7e1906255
Farzad Nasrolahy <farzadnasrolahy@gmail.com>
2025-04-29 21:52:43 +0330

    Reqular Expression Matching

---
+++ Regular Expression Matching/Regular Expression Matching/Program.cs
@@ -0,0 +1,11 @@
+﻿using System.Text.RegularExpressions;
+
+var s = new Solution().IsMatch("aaa","ab*a*c*a");
+Console.WriteLine(s);
+
+
+
+public class Solution
+{
+    public bool IsMatch(string s, string p) => Regex.IsMatch(s, "^" + p + "$");
+}
