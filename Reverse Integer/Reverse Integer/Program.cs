ad0cab0fe4d537f4d79286aa317f4cb4cb8a689a
Farzad Nasrolahy <farzadnasrolahy@gmail.com>
2025-04-29 15:47:25 +0330

    Reverse Integer

---
+++ Reverse Integer/Reverse Integer/Program.cs
@@ -0,0 +1,11 @@
+﻿public class Solution
+{
+    public int Reverse(int x)
+    {
+        var number = x.ToString();
+        var isNegative = number.StartsWith('-');
+        var reversedNumber = string.Join("", number.Replace("-", "").Reverse());
+        var converted = int.TryParse(isNegative ? "-" + reversedNumber : reversedNumber, out var result);
+        return converted ? result : 0;
+    }
+}
