39ef398516b39b4047287b6e2d3b9aeec4cb8a37
Farzad Nasrolahy <farzadnasrolahy@gmail.com>
2025-04-29 17:42:53 +0330

    Palindrome Number

---
+++ Palindrome Number/Palindrome Number/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Solution
+{
+    //public bool IsPalindrome(int x) => x.ToString() == new string(x.ToString().Reverse().ToArray()); //Via Stringify
+    public bool IsPalindrome(int x)
+    {
+        if (x < 0) return false; //Negative numbers cannot be palindromes 13ms
+        int temp = x;
+        int reverse = 0;
+        while (temp > 0)
+        {
+            int digit = temp % 10;
+            reverse = reverse * 10 + digit;
+            temp /= 10;
+        }
+        return x == reverse;
+    }//2ms
+}
