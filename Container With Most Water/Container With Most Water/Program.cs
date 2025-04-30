d44985016bac61e795ce800751e2047214b197b7
Farzad Nasrolahy <farzadnasrolahy@gmail.com>
2025-04-30 14:54:13 +0330

    Container With Most Water

---
+++ Container With Most Water/Container With Most Water/Program.cs
@@ -0,0 +1,20 @@
+﻿public class Solution
+{
+    public int MaxArea(int[] height)
+    {
+        int maxArea = 0,left = 0,right = height.Length - 1;
+
+        while(left < right)
+        {
+            var area = (right - left) * Math.Min(height[left], height[right]);
+            maxArea = Math.Max(maxArea, area);
+
+            if (height[left] < height[right])
+                left++;
+            else
+                right--;
+        }
+        
+        return maxArea;
+    }
+}
