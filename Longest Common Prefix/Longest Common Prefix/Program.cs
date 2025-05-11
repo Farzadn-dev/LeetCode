b8d28b3315302f4af3816f8bad9fce1a3e3a0ee1
Farzad Nasrolahy <farzadnasrolahy@gmail.com>
2025-05-11 11:43:16 +0330

    Longest Common Prefix

---
+++ Longest Common Prefix/Longest Common Prefix/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Text;
+
+public class Solution
+{
+    public string LongestCommonPrefix(string[] strs)
+    {
+        switch (strs.Length)
+        {
+            case 0:
+                return "";
+            case 1:
+                return strs[0];
+        }
+
+        var sb = new StringBuilder();
+        for (int i = 0; i < strs[0].Length; i++)
+        {
+            char c = strs[0][i];
+            for (int j = 1; j < strs.Length; j++)
+            {
+                if (i >= strs[j].Length || strs[j][i] != c)
+                    return sb.ToString();
+            }
+            sb.Append(c);
+        }
+        return sb.ToString();
+    }
+}
