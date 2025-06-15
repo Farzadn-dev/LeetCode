81eadd0e4f6a018290b381e0bec9b418f6f3d7b1
Farzad Nasrolahy <farzadnasrolahy@gmail.com>
2025-06-15 11:21:54 +0330

    Valid Parentheses

---
+++ Valid Parentheses/Valid Parentheses/Program.cs
@@ -0,0 +1,38 @@
+﻿public class Solution
+{
+    public bool IsValid(string s)
+    {
+        // Use stackalloc for performance and avoid heap allocations
+        Span<char> stack = stackalloc char[s.Length];
+        int top = -1;
+
+        foreach (var c in s)
+        {
+            switch (c)
+            {
+                case '(':
+                case '{':
+                case '[':
+                    stack[++top] = c;
+                    continue;
+            }
+
+            if (top < 0)
+                return false;
+
+            char open = stack[top--];
+
+            if (!IsMatchingPair(open, c))
+                return false;
+        }
+
+        return top == -1;
+    }
+
+    private bool IsMatchingPair(char open, char close)
+    {
+        return (open == '(' && close == ')') ||
+               (open == '{' && close == '}') ||
+               (open == '[' && close == ']');
+    }
+}
