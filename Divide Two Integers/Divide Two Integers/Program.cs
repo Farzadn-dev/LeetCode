e2be93837359ef8aa2a4cf519bf228b0022362c7
Farzad <Farzadnasrolahy@gmail.com>
2025-06-28 17:02:01 +0330

    Divide Two Integers

---
+++ Divide Two Integers/Divide Two Integers/Program.cs
@@ -0,0 +1,14 @@
+﻿public class Solution
+{
+    public int Divide(int dividend, int divisor)
+    {
+        var res = (Int64)dividend / divisor;
+        if (res > int.MaxValue)
+            return int.MaxValue;
+
+        if (res < int.MinValue)
+            return int.MinValue;
+
+        return (int)res;
+    }
+}
