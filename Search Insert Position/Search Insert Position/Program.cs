b893216b5f4119e3bfcbb00f41ef4824d395e8d9
Farzad Nasrolahy <farzadnasrolahy@gmail.com>
2025-07-14 14:47:51 +0330

    Search Insert Position

---
+++ Search Insert Position/Search Insert Position/Program.cs
@@ -0,0 +1,23 @@
+﻿public class Solution
+{
+    public int SearchInsert(int[] nums, int target)
+    {
+        int mid = 0, left = 0, right = nums.Length - 1;
+        while (left <= right)
+        {
+            mid = left + (right - left) / 2;
+            if (nums[mid] == target)
+                return mid;
+
+            //Target is in left Side
+            if (nums[mid] > target)
+                right = mid - 1;
+
+            //Target is in right side
+            else
+                left = mid + 1;
+        }
+
+        return nums[mid] > target ? mid : mid + 1;
+    }
+}
