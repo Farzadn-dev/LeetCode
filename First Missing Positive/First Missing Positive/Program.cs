342f7e08a4074c464c8bbc9a65146a342d77f883
Farzad Nasrolahy <farzadnasrolahy@gmail.com>
2025-07-21 15:52:40 +0330

    First Missing Positive

---
+++ First Missing Positive/First Missing Positive/Program.cs
@@ -0,0 +1,30 @@
+﻿//This Code is Optimized by AI
+public class Solution
+{
+    public int FirstMissingPositive(int[] nums)
+    {
+        int n = nums.Length;
+
+        // Place numbers in their correct index
+        for (int i = 0; i < n; i++)
+        {
+            while (nums[i] > 0 && nums[i] <= n && nums[nums[i] - 1] != nums[i])
+            {
+                // Swap nums[i] with nums[nums[i] - 1]
+                int temp = nums[nums[i] - 1];
+                nums[nums[i] - 1] = nums[i];
+                nums[i] = temp;
+            }
+        }
+
+        // First index where number is not index + 1
+        for (int i = 0; i < n; i++)
+        {
+            if (nums[i] != i + 1)
+                return i + 1;
+        }
+
+        // If all are in place
+        return n + 1;
+    }
+}
