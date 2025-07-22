e423fe686219f789be84f1cfc1ec0c3bbc9ef065
Farzad Nasrolahy <farzadnasrolahy@gmail.com>
2025-07-22 16:54:17 +0330

    Trapping Rain Water

---
+++ Trapping Rain Water/Trapping Rain Water/Program.cs
@@ -0,0 +1,30 @@
+﻿public class Solution
+{
+    public int Trap(int[] height)
+    {
+        int left = 0, right = height.Length - 1;
+        int leftMax = height[left], rightMax = height[right];
+        int water = 0;
+        while (left < right)
+        {
+            if (height[right] <= height[left])
+            {
+                right--;
+                if (rightMax >= height[right])
+                    water += rightMax - height[right];
+                else
+                    rightMax = height[right];
+            }
+            else
+            {
+                left++;
+                if (leftMax >= height[left])
+                    water += leftMax - height[left];
+                else
+                    leftMax = height[left];
+            }
+        }
+
+        return water;
+    }
+}
